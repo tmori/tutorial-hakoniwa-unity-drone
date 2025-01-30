f7866ea19b6ecf15f2216772d261237d2a05dfa5
Takashi Mori <kanetugu2015@gmail.com>
2025-01-30 18:13:35 +0900

    add baggage for new physics

--- drone-simulation/Assets/Scripts/ARBridge/ShareSim/Baggage/BaggagePhysics.cs
+++ drone-simulation/Assets/Scripts/ARBridge/ShareSim/Baggage/BaggagePhysics.cs
@@ -1,16 +1,34 @@
+using hakoniwa.ar.bridge.sharesim;
+using hakoniwa.objects.core;
+using hakoniwa.pdu.msgs.hako_msgs;
 using UnityEngine;
 
-public class BaggagePhysics : MonoBehaviour
+public class BaggagePhysics : MonoBehaviour, IShareSimPhysics
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Baggage baggage;
+
+    public void Initialize(GameObject target)
     {
-        
+        baggage = target.GetComponentInChildren<Baggage>();
+        if (baggage == null)
+        {
+            throw new System.Exception("Can not find baggage on " + this.transform.name);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartPhysics()
     {
-        
+        //nothing to do
     }
+
+    public void StopPhysics()
+    {
+        //nothing to do
+    }
+
+    public void UpdatePosition(ShareObjectOwner owner)
+    {
+        //nothing to do
+    }
+
 }
